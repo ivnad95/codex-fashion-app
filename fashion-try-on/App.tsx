43bcc2f6df4e640d8ef727c72672cad72e140393
ivnad95 <107779994+ivnad95@users.noreply.github.com>
2025-09-01 20:24:01 +0100

    chore: tune babel plugins

--- fashion-try-on/App.tsx
+++ fashion-try-on/App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ReactNode } from 'react';
 
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
@@ -9,6 +10,10 @@ import { RootStackParamList, RootTabParamList } from './src/navigation/types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<RootTabParamList>();
+
+function ImageProvider({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
 
 function Tabs() {
   return (
@@ -21,11 +26,13 @@ function Tabs() {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ImageProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ImageProvider>
   );
 }
