4d16ef4ca39a030cae5c44b0ea07da57b293bab2
ivnad95 <107779994+ivnad95@users.noreply.github.com>
2025-09-01 18:44:07 +0100

    fix: guard EAS builds and configure jest-native

--- fashion-try-on/jest.config.mjs
+++ fashion-try-on/jest.config.mjs
@@ -1,6 +1,6 @@
 export default {
   preset: 'jest-expo',
-  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-native-svg|@react-navigation/.*)'
   ]
