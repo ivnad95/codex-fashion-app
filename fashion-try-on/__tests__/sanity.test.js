6d9cfe160f4871167989e85113b000036092de4a
ivnad95 <107779994+ivnad95@users.noreply.github.com>
2025-09-06 17:05:12 +0100

    fix: use tailwind for blur background

--- fashion-try-on/__tests__/sanity.test.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports, no-undef */
-const test = require('node:test');
-const assert = require('node:assert/strict');
-
-test('math works', () => {
-  assert.equal(1 + 1, 2);
-});
